c4e8a82c8a84ef8cb932891301dedf4bb2e2f5f8
ghiscoding <gbeaulac@gmail.com>
2023-02-21 19:54:33 -0500

    chore: add auto dropdown width to demo

--- demo/src/options/options30.ts
+++ demo/src/options/options30.ts
@@ -4,6 +4,7 @@ export default class Example {
   ms1?: MultipleSelectInstance;
   ms2?: MultipleSelectInstance;
   ms3?: MultipleSelectInstance;
+  ms4?: MultipleSelectInstance;
 
   mount() {
     this.ms1 = multipleSelect('#select1', {
@@ -14,18 +15,18 @@ export default class Example {
     }) as MultipleSelectInstance;
 
     this.ms2 = multipleSelect('#select2', {
-      autoAdjustDropWidthByTextSize: true,
       autoAdjustDropHeight: true,
       position: 'top',
       showOkButton: true,
     }) as MultipleSelectInstance;
 
     this.ms3 = multipleSelect('#select3', {
-      autoAdjustDropWidthByTextSize: true,
       autoAdjustDropHeight: true,
       filter: true,
       position: 'top',
     }) as MultipleSelectInstance;
+
+    this.ms4 = multipleSelect('#select4') as MultipleSelectInstance;
   }
 
   unmount() {
@@ -33,8 +34,10 @@ export default class Example {
     this.ms1?.destroy();
     this.ms2?.destroy();
     this.ms3?.destroy();
+    this.ms4?.destroy();
     this.ms1 = undefined;
     this.ms2 = undefined;
     this.ms3 = undefined;
+    this.ms4 = undefined;
   }
 }
