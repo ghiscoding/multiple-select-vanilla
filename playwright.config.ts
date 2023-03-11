d68f649a3179fe6f6048caabd05c1c565a5e05b1
ghiscoding <gbeaulac@gmail.com>
2023-03-11 18:15:13 -0500

    chore: Playwright testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: 'e2e',
   use: {
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     video: 'on-first-retry',
