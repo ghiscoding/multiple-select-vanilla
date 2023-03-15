2f0076546ff390dc3649850a64588c05c373228e
ghiscoding <gbeaulac@gmail.com>
2023-03-14 23:21:37 -0400

    chore: improve Playwright E2E tests & report

--- playwright/e2e/example01.spec.ts
+++ playwright/e2e/example01.spec.ts
@@ -5,14 +5,14 @@ test.describe('Example 01 - Basic Select', () => {
     await page.goto('#/example01');
 
     await page.locator('div[data-test=select1] .ms-choice').filter({ hasText: 'First' }).click();
-    await expect(await page.locator('div[data-test=select1] .ms-drop li.hide-radio').count()).toBe(4);
+    await expect(page.locator('div[data-test=select1] .ms-drop li.hide-radio')).toHaveCount(4);
     await page.locator('div[data-test=select1] .ms-drop li label').filter({ hasText: 'Fourth' }).click();
 
     const optGroups = await page.locator('.group.hide-radio .optgroup.disabled');
     const liElms = await page.locator('div[data-test=select2] .option-level-1');
     await expect(optGroups).toBeDefined();
-    await expect(await optGroups.count()).toBe(3);
-    await expect(await liElms.count()).toBe(9);
+    await expect(optGroups).toHaveCount(3);
+    await expect(liElms).toHaveCount(9);
     await expect(optGroups.nth(0)).toContainText('Group 1');
     await expect(optGroups.nth(1)).toContainText('Group 2');
     await expect(optGroups.nth(2)).toContainText('Group 3');
