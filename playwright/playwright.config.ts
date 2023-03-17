4f628938e44a9d60ce9b6df688012c2930147350
ghiscoding <gbeaulac@gmail.com>
2023-03-17 00:27:14 -0400

    chore: update playwright report to gh-pages

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   fullyParallel: true,
   // reporter: process.env.CI ? 'dot' : 'list',
-  reporter: [['html', { outputFolder: '../docs/playwright-report' }]],
+  reporter: [['html', { outputFolder: 'playwright-report' }]],
   testDir: './e2e',
   timeout: 30 * 1000,
   expect: {
