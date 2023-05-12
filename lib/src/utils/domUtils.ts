02c26bc1ba87a08a80c2ae71c5d6782e6c7afddf
ghiscoding <gbeaulac@gmail.com>
2023-05-12 00:13:45 -0400

    fix: create DOM element util should spread element props - also add 3rd argument for a parent element to optionally append to

--- lib/src/utils/domUtils.ts
+++ lib/src/utils/domUtils.ts
@@ -37,10 +37,20 @@ export function calculateAvailableSpace(element: HTMLElement): { top: number; bo
   return { top, bottom, left, right };
 }
 
-/** Create a DOM Element with any optional attributes or properties */
+/**
+ * Create a DOM Element with any optional attributes or properties.
+ * It will only accept valid DOM element properties that `createElement` would accept.
+ * For example: `createDomElement('div', { className: 'my-css-class' })`,
+ * for style or dataset you need to use nested object `{ style: { display: 'none' }}
+ * The last argument is to optionally append the created element to a parent container element.
+ * @param {String} tagName - html tag
+ * @param {Object} options - element properties
+ * @param {[HTMLElement]} appendToParent - parent element to append to
+ */
 export function createDomElement<T extends keyof HTMLElementTagNameMap, K extends keyof HTMLElementTagNameMap[T]>(
   tagName: T,
-  elementOptions?: { [P in K]: InferDOMType<HTMLElementTagNameMap[T][P]> }
+  elementOptions?: { [P in K]: InferDOMType<HTMLElementTagNameMap[T][P]> },
+  appendToParent?: HTMLElement
 ): HTMLElementTagNameMap[T] {
   const elm = document.createElement<T>(tagName);
 
@@ -48,11 +58,14 @@ export function createDomElement<T extends keyof HTMLElementTagNameMap, K extend
     Object.keys(elementOptions).forEach((elmOptionKey) => {
       const elmValue = elementOptions[elmOptionKey as keyof typeof elementOptions];
       if (typeof elmValue === 'object') {
-        Object.assign({}, elm[elmOptionKey as K], elmValue);
+        Object.assign(elm[elmOptionKey as K] as object, elmValue);
       } else {
         elm[elmOptionKey as K] = (elementOptions as any)[elmOptionKey as keyof typeof elementOptions];
       }
     });
+  }
+  if (appendToParent && appendToParent.appendChild) {
+    appendToParent.appendChild(elm);
   }
   return elm;
 }
