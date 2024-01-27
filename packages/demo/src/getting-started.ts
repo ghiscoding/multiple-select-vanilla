1d4aee705e7cff952ab16e5e5cc178a1dd22c2bd
ghiscoding <gbeaulac@gmail.com>
2024-01-27 15:52:29 -0500

    chore: remove unused code

--- packages/demo/src/getting-started.ts
+++ packages/demo/src/getting-started.ts
@@ -1,10 +1,1 @@
-import { multipleSelect } from 'multiple-select-vanilla';
-
-export default class Example {
-  mount() {
-    multipleSelect('#select1');
-    multipleSelect('#select2', {
-      placeholder: 'Here is the placeholder via javascript',
-    });
-  }
-}
+export default class Example {}
