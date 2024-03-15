42c87f76954f3eabb0593a33cdf6cdb060dc487a
Ghislain B <gbeaulac@gmail.com>
2024-03-14 23:41:51 -0400

    fix: select drop caret icon not flipping vertically when opened (#243)

--- playwright/e2e/example15.spec.ts
+++ playwright/e2e/example15.spec.ts
@@ -7,14 +7,18 @@ test.describe('Example 15 - Dark Mode', () => {
     await page.locator('div[data-test=single] .ms-choice').filter({ hasText: 'January' }).click();
     const li1LabelElms = await page.locator('div[data-test=single] li.hide-radio');
     const li1DividerElms = await page.locator('div[data-test=single] li.option-divider');
+    expect(await page.locator('div[data-test=single] div.ms-icon-caret')).toHaveClass(/open/);
     await page.locator('div[data-test=single] .ms-drop li label').filter({ hasText: 'March' }).click();
+    expect(await page.locator('div[data-test=single] div.ms-icon-caret')).not.toHaveClass(/open/);
     await expect(li1LabelElms).toHaveCount(12);
     await expect(li1DividerElms).toHaveCount(2);
 
     await page.locator('div[data-test=radio] .ms-choice').filter({ hasText: 'February' }).click();
     const li2LabelElms = await page.locator('div[data-test=radio] li[role=option]');
     const li2DividerElms = await page.locator('div[data-test=radio] li.option-divider');
+    expect(await page.locator('div[data-test=radio] div.ms-icon-caret')).toHaveClass(/open/);
     await page.locator('div[data-test=radio] .ms-drop li label').filter({ hasText: 'April' }).click();
+    expect(await page.locator('div[data-test=radio] div.ms-icon-caret')).not.toHaveClass(/open/);
     await expect(li2LabelElms).toHaveCount(12);
     await expect(li2DividerElms).toHaveCount(2);
 
@@ -23,9 +27,11 @@ test.describe('Example 15 - Dark Mode', () => {
     const li3DividerElms = await page.locator('div[data-test=multiple] li.option-divider');
     await expect(li3LabelElms).toHaveCount(13);
     await expect(li3DividerElms).toHaveCount(2);
+    expect(await page.locator('div[data-test=multiple] div.ms-icon-caret')).toHaveClass(/open/);
     await page.locator('div[data-test=multiple] span').filter({ hasText: 'February' }).click();
     const parent2Span = await page.locator('div[data-test=multiple] .ms-choice span');
     await page.getByRole('button', { name: 'February' }).click();
+    expect(await page.locator('div[data-test=multiple] div.ms-icon-caret')).not.toHaveClass(/open/);
     await expect(parent2Span).toHaveText('February');
 
     await page.locator('div[data-test=group] .ms-choice').click();
@@ -34,9 +40,11 @@ test.describe('Example 15 - Dark Mode', () => {
     await expect(li4LabelElms).toHaveCount(15);
     await expect(li4DividerElms).toHaveCount(4);
     await page.getByText('Group 1').click();
+    expect(await page.locator('div[data-test=group] div.ms-icon-caret')).toHaveClass(/open/);
     await page.locator('div[data-test=group].ms-parent').click();
     const parent3Span = await page.locator('div[data-test=group] .ms-choice span');
     await expect(parent3Span).toHaveText('6 of 12 selected');
+    expect(await page.locator('div[data-test=group] div.ms-icon-caret')).not.toHaveClass(/open/);
 
     await page.locator('div[data-test=data1] .ms-choice').click();
     const li5LabelElms = await page.locator('div[data-test=data1] li:not(.option-divider)');
@@ -45,7 +53,9 @@ test.describe('Example 15 - Dark Mode', () => {
     await expect(li5DividerElms).toHaveCount(1);
     await page.locator('div[data-test=data1] span').filter({ hasText: 'Option 2' }).click();
     const parent4Span = await page.locator('div[data-test=data1] .ms-choice span');
+    expect(await page.locator('div[data-test=data1] div.ms-icon-caret')).toHaveClass(/open/);
     await page.getByRole('button', { name: 'Option 2' }).click();
     await expect(parent4Span).toHaveText('Option 2');
+    expect(await page.locator('div[data-test=data1] div.ms-icon-caret')).not.toHaveClass(/open/);
   });
 });
