d21fb2b3de4b9df86aee5bf359872ead74549a44
silvester-pari <silvester.pari@eox.at>
2024-05-22 08:10:56 +0200

    chore: undo unnecessary change

--- packages/demo/src/main.ts
+++ packages/demo/src/main.ts
@@ -154,7 +154,7 @@ class Main {
     this.removeAllActiveLinks();
 
     // change active link to new route
-    const targetElm = e.composedPath()[0] as HTMLElement;
+    const targetElm = e.target as HTMLElement;
     targetElm.classList.toggle('active');
 
     this.loadRoute(targetElm.id);
