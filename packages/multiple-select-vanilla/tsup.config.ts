2c10ebdd2a71594528c86478a4268565ed0cf5cf
Ghislain B. <gbeaulac@gmail.com>
2024-10-04 17:54:57 -0400

    chore: tsup minify build output

--- packages/multiple-select-vanilla/tsup.config.ts
+++ packages/multiple-select-vanilla/tsup.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(options => [
     sourcemap: true,
     // clean: true,
     dts: !options.watch,
+    minify: !options.watch,
     outExtension: ({ format }) => ({ js: format === 'cjs' ? '.cjs' : '.mjs' }),
   },
   // common js for node and other backend runtimes
@@ -19,6 +20,7 @@ export default defineConfig(options => [
     sourcemap: true,
     // clean: true,
     dts: !options.watch,
+    minify: true,
     outExtension: ({ format }) => ({
       js: format === 'cjs' ? '.cjs' : '.mjs',
     }),
