395ff6313dcb19300dd3e4537a4e764e3548de01
ghiscoding <gbeaulac@gmail.com>
2025-04-19 01:59:53 -0400

    chore: add min delay to init drop

--- packages/multiple-select-vanilla/src/MultipleSelectInstance.ts
+++ packages/multiple-select-vanilla/src/MultipleSelectInstance.ts
@@ -371,7 +371,7 @@ export class MultipleSelectInstance {
     this.update(true);
 
     if (this.options.isOpen) {
-      this.open(null);
+      this.open(0);
     }
 
     if (this.options.openOnHover && this.parentElm) {
