146bd953a61c6d34c634515550901a2cdceca7ed
ghiscoding <gbeaulac@gmail.com>
2025-10-16 22:10:16 -0400

    chore: fix failing playwright test

--- playwright/e2e/options26.spec.ts
+++ playwright/e2e/options26.spec.ts
@@ -28,7 +28,7 @@ test.describe('Options 26 - The Styler / CSS Styler', () => {
     optionLoc3.click();
     expect(optionLoc3).toHaveText('Option 3');
     await expect(optionLoc3).toHaveCSS('color', 'rgb(128, 0, 128)');
-    await expect(optionLoc3).toHaveCSS('text-decoration', 'underline solid rgb(128, 0, 128)');
+    await expect(optionLoc3).toHaveCSS('text-decoration', 'underline');
     const dropLoc2 = await page.locator('[data-test=select4] .ms-choice span', { hasText: '[Group 1: Option 3]' });
     await dropLoc2.waitFor();
   });
