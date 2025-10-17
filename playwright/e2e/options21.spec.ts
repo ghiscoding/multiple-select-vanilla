82a1ac7a97693c6dd333bc2b4173945400bc5d41
ghiscoding <gbeaulac@gmail.com>
2025-10-16 22:20:46 -0400

    chore: try different Enter key press

--- playwright/e2e/options21.spec.ts
+++ playwright/e2e/options21.spec.ts
@@ -7,14 +7,14 @@ test.describe('Options 21 - Filter Accept On Enter', () => {
     await page.getByRole('textbox', { name: '🔎︎' });
     await page.keyboard.type('ef');
     await expect(page.locator('.ms-drop li.hide-radio')).toHaveCount(2);
-    await page.keyboard.press('Enter');
+    await page.getByRole('textbox').press('Enter');
     await expect(page.locator('[data-test=select1] .ms-choice span')).toHaveText('def');
 
     await page.locator('[data-test=select2].ms-parent').click();
     await page.getByRole('textbox', { name: '🔎︎' });
     await page.keyboard.type('g');
     await expect(page.locator('.ms-drop li.hide-radio')).toHaveCount(2);
-    await page.keyboard.press('Enter');
+    await page.getByRole('textbox').press('Enter');
     await expect(page.locator('[data-test=select2] .ms-choice span')).toHaveText('efg, fgh, ghi');
   });
 });
