23980ec95aa73b607d811f491e3f7e276356ef8d
Ghislain B. <gbeaulac@gmail.com>
2025-11-06 21:33:56 -0500

    chore: tweak some more

--- playwright/e2e/example16.spec.ts
+++ playwright/e2e/example16.spec.ts
@@ -7,6 +7,7 @@ test.describe('Example 16 - Avatar Images as Text Template', () => {
     // 1st select
     const parentSelect1 = await page.locator('.ms-parent[data-test=select1]');
     await parentSelect1.click();
+    await expect(page.locator('div[data-test=select1] .ms-drop')).not.toBeHidden();
     const parentSelectList1 = await page.locator('.ms-parent[data-test=select1] .ms-list');
     await parentSelectList1.press('ArrowDown', { delay: 10});
     await parentSelectList1.press('ArrowDown', { delay: 10});
@@ -20,9 +21,12 @@ test.describe('Example 16 - Avatar Images as Text Template', () => {
     // 2nd select
     const parentSelect2 = await page.locator('.ms-parent[data-test=select2]');
     await parentSelect2.click();
+    await expect(page.locator('div[data-test=select2] .ms-drop')).not.toBeHidden();
     const parentSelectList2 = await page.locator('.ms-parent[data-test=select2] .ms-list');
     await parentSelectList2.press('ArrowDown', { delay: 10});
     await parentSelectList2.press('Enter');
+    await expect(page.locator('[data-test=select2].ms-drop li.selected').nth(0).locator('div.name-section')).toHaveText('Smith');
+    await expect(page.locator('[data-test=select2].ms-drop li.selected').nth(0).locator('div.job-section')).toHaveText('Accounting');
     const parentSpan2 = await page.locator('.ms-parent[data-test=select2] .ms-choice span');
     expect(await parentSpan2.locator('.name-section')).toHaveText('Smith');
     expect(await parentSpan2.locator('.job-section')).toHaveText('Accounting');
