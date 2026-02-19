86d9fd6056b1f267a5be477cd50cbea0df91d9f7
ghiscoding <gbeaulac@gmail.com>
2026-02-18 19:36:21 -0500

    feat: add tabIndex option to button.ms-choice

---
+++ packages/demo/src/options/options44.ts
@@ -0,0 +1,17 @@
+import { type MultipleSelectInstance, multipleSelect } from 'multiple-select-vanilla';
+
+export default class Example {
+  ms1?: MultipleSelectInstance;
+
+  mount() {
+    this.ms1 = multipleSelect('select', {
+      tabIndex: 0,
+    }) as MultipleSelectInstance;
+  }
+
+  unmount() {
+    // destroy ms instance(s) to avoid DOM leaks
+    this.ms1?.destroy();
+    this.ms1 = undefined;
+  }
+}
