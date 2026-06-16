d7a1e817352fe1b6742a45e8da85e1156928220a
ghiscoding <gbeaulac@gmail.com>
2026-06-15 20:29:54 -0400

    refactor: simplify DOM utils with modern code logic

--- packages/multiple-select-vanilla/src/MultipleSelectInstance.ts
+++ packages/multiple-select-vanilla/src/MultipleSelectInstance.ts
@@ -1341,7 +1341,7 @@ export class MultipleSelectInstance {
           container = this.options.container === 'body' ? document.body : (document.querySelector(this.options.container) as HTMLElement);
         }
         container!.appendChild(this.dropElm);
-        const { top: offsetTop = 0, left: offsetLeft = 0 } = getOffset(this.parentElm) || {};
+        const { top: offsetTop, left: offsetLeft } = getOffset(this.parentElm);
         this.dropElm.style.top = `${offsetTop + this.parentElm.offsetHeight}px`;
         this.dropElm.style.left = `${offsetLeft}px`;
         this.dropElm.style.width = `${this.parentElm.offsetWidth}px`;
